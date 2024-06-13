091592d19e4158302dab66273cf744bd4913a4a0
VanChanh <vanchanh0730@gmai.com>
2024-06-13 23:58:48 +0700

    complete UI

--- Frontend/src/components/sidebar/SideBar.jsx
+++ Frontend/src/components/sidebar/SideBar.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import SearchInput from "./SearchInput";
+import ManyConversation from "./ManyConversation";
+import LogoutButton from "./LogoutButton";
 
 const SideBar = () => {
   return (
-    <div>
+    <div className="border-r border-slate-500 p-4 flex flex-col">
       <SearchInput />
       <div className="divider px-3"></div>
+      <ManyConversation />
+
+      <LogoutButton />
     </div>
   );
 };
