fb29d87ce0cb89c537265af8c04b686d56ba4ebe
VanChanh <vanchanh0730@gmai.com>
2024-06-21 14:40:03 +0700

    send message and get message

--- Frontend/src/components/messages/ManyMessages.jsx
+++ Frontend/src/components/messages/ManyMessages.jsx
@@ -1,20 +1,25 @@
 import React from "react";
 import Message from "./Message";
+import useGetMessages from "../../hooks/useGetMessages";
+import MessageSkeleton from "../skeletons/MessageSkeleton";
 
 const ManyMessages = () => {
+  const { loading, messages } = useGetMessages();
   return (
     <div className="px-4 flex-1 overflow-auto">
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
+      {!loading &&
+        messages.length > 0 &&
+        messages.map((message) => (
+          <Message key={message._id} message={message} />
+        ))}
+
+      {loading && [...Array(3)].map((_, idx) => <MessageSkeleton key={idx} />)}
+
+      {!loading && messages.length === 0 && (
+        <p className="text-center text-warning">
+          Send a message to start the conversation
+        </p>
+      )}
     </div>
   );
 };
--- Frontend/src/components/messages/MessageContainer.jsx
+++ Frontend/src/components/messages/MessageContainer.jsx
@@ -1,21 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ManyMessages from "./ManyMessages";
 import MessageInput from "./MessageInput";
 import { TiMessage } from "react-icons/ti";
+import useConversation from "../../zustand/useConversation";
 
 const MessageContainer = () => {
-  const noChatSelected = true;
+  const { selectedConversation, setSelectedConversation } = useConversation();
+
+  useEffect(() => {
+    return () => setSelectedConversation(null);
+  }, [setSelectedConversation]);
 
   return (
     <div className="md:min-w-[450px] flex flex-col">
-      {noChatSelected ? (
+      {!selectedConversation ? (
         <NoChatSelected />
       ) : (
         <>
           {/* Header */}
           <div className="bg-slate-500 px-4 py-2 mb-2">
             <span className="label-text font-bold">To: </span>
-            <span className="text-gray-900 font-bold">John Doe</span>
+            <span className="text-gray-900 font-bold">
+              {selectedConversation.fullName}
+            </span>
           </div>
 
           <ManyMessages />
